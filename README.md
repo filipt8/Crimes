568838bfe71efd5e4dd283b1284776b91c065bec
filipt8 <filiptrochimiuk8@gmail.com>
2024-06-24 16:38:09 +0200

    aktaualizacja + chatbot

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 crimes.py - aplikacja w streamlit
 plots.py - funkcje rysujące wykresy
 caloscplot.py - kod do utworzenia top5plot.png
+model_crimes_raport.ipynb - proces uczenia modelu
+model_crimes_raport.html - raport z procesu uczenia
 
 
 Start aplikacji:
 *python -m streamlit run crimes.py*
+
+lub
+
+*streamlit run crimes.py*
--- crimes.py
+++ crimes.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from plots import load_data, months, days, hours, allthetime, crimesline, geo_crime, percentage, area
 import plotly.express as px
-st.set_option('deprecation.showPyplotGlobalUse', False)
+import joblib
+import time
 
 st.title('Analiza danych kryminalnych')
 dane = load_data()
 
+####################################################################################
 
 if 'button_clicked' not in st.session_state:
     st.session_state.button_clicked = None
@@ -21,8 +23,8 @@ if st.sidebar.button('ZBIÓR'):
 if st.sidebar.button('WYKRESY'):
     st.session_state.button_clicked = 'wykresy'
 
-if st.sidebar.button('TEST'):
-    st.session_state.button_clicked = 'test'
+if st.sidebar.button('CHATBOT'):
+    st.session_state.button_clicked = 'Chatbot'
 
 #####################################################################################
 
@@ -68,9 +70,13 @@ if st.session_state.button_clicked == 'opis_zbioru':
         elif st.session_state.genre == "***Opis zbioru i zmiennych***":
             st.header("Crimes in Los Angeles")
             st.write(f"""
-                    Zbiór przestępstw popełnionych od początku 2010r. do września 2017r.
-                    Znajdują się w nim dane dotyczące przestępstw i wykroczeń na terenie całego Los Angeles.
-                    Zajmiemy się następującymi kwestiami:
+                    Aplikacja obsługuje zbiór przestępstw popełnionych od początku 2010r. do września 2017r. na terenie całego Los Angeles.
+                    
+                    Dane w zbiorze są bardzo zróżnicowane, znaleźć w nich możemy przestępstwo dotyczące wykolejenia pociągu, które miało miejsce raz przez okres prawie 7 lat podczas gdy BATTERY - SIMPLE ASSAULT czyli próba wymuszenia 
+                    'niepożądanego dotyku' drugiej osoby była odnotowana aż 145767 razy.
+                     
+                    Dane zawierają dosyć szczegółowe informacje na temat lokalizacji miejsca przestępstwa, czasu jego dokonania, ofiary, narzędzia zbrodni czy kodu Modus Operandi (charakterystyczna metoda, wzorzec lub technika działania).
+
                     Link do źródła danych [kaggle.com]({url})
             """)
             st.subheader("Opis zmiennych")
@@ -95,7 +101,7 @@ if st.session_state.button_clicked == 'opis_zbioru':
 
                     Kod broni użytej podczas interwencji wraz z opisem - `Weapon Used Code`, `Weapon Description`
 
-                    Kod opisujący zakończenie akcji - `Status Description`
+                    Kod opisujący zakończenie akcji - ``Status Code, `Status Description`
 
                     Adres zdarzenia - `Address`, `Cross Street`, `Location` 
             """)
@@ -186,19 +192,110 @@ elif st.session_state.button_clicked == 'wykresy':
             result = geo_crime(dane, crimes_filter)
             st.pyplot(result)
             
+if st.session_state.button_clicked == 'Chatbot':
+    model = joblib.load('random_forest_model_crimes_10k_2.pkl')
+    preprocessor = joblib.load('preprocessor_crimes_10k_2.pkl')
 
-#WSZĘDZIE MOŻESZ DODAĆ st.sidebar.selectbox CZY COŚ ŻEBY TO MIAŁO RĘCE I NOGI
-#np. DLA MIESIĘCY... DLA POSZCZEGÓLNEGO PRZESTĘPSTWA - TAK SAMO PŁEĆ ITD
-#MOŻNA DODAĆ TEKSTOWĄ CIEKAWOSTKE, ŻE NP. NAJMŁODSZA OFIARA BYŁA BIAŁA WIEK TYLE A NAJSTARSZA TYLE.
+    dane['Weapon Used Code'] = dane['Weapon Used Code'].fillna(0)
+    dane['Weapon Description'] = dane['Weapon Description'].fillna('NO WEAPON USED')
+    dane['MO Codes'] = dane['MO Codes'].fillna('')
+    data = dane.dropna(subset=['Area Name', 'Premise Description', 'Status Description', 'Victim Sex', 'Victim Descent'])
 
-#PAMIETAJ JAKIEŚ ZJEBANE MODELE NA SZYBKO SKLEIĆ ZE 3 I POKAZAĆ ICH PLOTY
+    mo_codes_data = pd.read_csv('MO_Codes.csv')
 
-#MOŻESZ DOROBIĆ DO TEGO CZATBOTA, KTÓRY NA PODSTAWIE PODANYCH INFORMACJI SPRÓBUJE PRZEWIDZIEĆ KTO POPEŁNIŁ PRZESTĘPSTWO
-#ALE TO JUŻ ZBYT POJEBANE CHYBA
-#COŚ W STYLU PROFILERA - ZROBIŁA TO I TO, Z BRONIĄ W RĘKU, W TYM MIEJSCU O TEJ GODZINIE
+    mo_codes_dict = dict(zip(mo_codes_data['Description'], mo_codes_data['MO_Code']))
+    mo_codes_unique = list(mo_codes_dict.keys())
 
-#####################################################################################################
+    bins = [0, 21, 29, 39, 51, 100]
+    labels = ['10-21', '22-29', '30-39', '40-51', '52-100']
+    data['Victim Age'] = pd.cut(data['Victim Age'], bins=bins, labels=labels, right=False)
 
-if st.session_state.button_clicked == 'test':
-    gender_filter = st.sidebar.selectbox('Filter by Gender', ['All', 'Male', 'Female'])
+    area_dict = dict(zip(data['Area ID'], data['Area Name']))
+    premise_dict = dict(zip(data['Premise Code'], data['Premise Description']))
+    weapon_dict = dict(zip(data['Weapon Used Code'], data['Weapon Description']))
+    status_dict = dict(zip(data['Status Code'], data['Status Description']))
+    
+    crime_code_dict = dict(zip(data['Crime Code'], data['Crime Code Description']))
+
+    victim_descent_dict = {
+        "A": "Other Asian", "B": "Black", "C": "Chinese", "D": "Cambodian",
+        "F": "Filipino", "G": "Guamanian", "H": "Hispanic/Latin/Mexican",
+        "I": "American Indian/Alaskan Native", "J": "Japanese", "K": "Korean",
+        "L": "Laotian", "O": "Other", "P": "Pacific Islander", "S": "Samoan",
+        "U": "Hawaiian", "V": "Vietnamese", "W": "White", "X": "Unknown", "Z": "Asian Indian"
+    }
+    victim_descent_unique = list(victim_descent_dict.values())
+    status_desc_unique = data['Status Description'].dropna().unique()
+
+    def typewriter(text: str, speed: int):
+        tokens = text.split()
+        container = st.empty()
+        for index in range(len(tokens) + 1):
+            curr_full_text = " ".join(tokens[:index])
+            container.markdown(curr_full_text)
+            time.sleep(1 / speed)
+
+    def predict_crime(victim_age, victim_sex, victim_descent, premise_desc, area_name, weapon_desc, time_occurred, mo_descs, status_desc):
+        premise_code = list(premise_dict.keys())[list(premise_dict.values()).index(premise_desc)]
+        area_id = list(area_dict.keys())[list(area_dict.values()).index(area_name)]
+        weapon_used_code = list(weapon_dict.keys())[list(weapon_dict.values()).index(weapon_desc)]
+        status_code = list(status_dict.keys())[list(status_dict.values()).index(status_desc)]
+        
+        # Join multiple MO Codes into a single string separated by commas
+        mo_codes = ','.join([str(mo_codes_dict[mo_desc]) for mo_desc in mo_descs])
+
+        input_data = pd.DataFrame({
+            'Victim Age': [victim_age],
+            'Victim Sex': [victim_sex],
+            'Victim Descent': [victim_descent],
+            'Premise Code': [premise_code],
+            'Area ID': [area_id],
+            'Weapon Used Code': [weapon_used_code],
+            'Time Occurred': [time_occurred],
+            'MO Codes': [mo_codes],
+            'Status Code': [status_code]
+        })
+
+        input_data['Victim Age'] = input_data['Victim Age'].astype(str)
+        input_data['Victim Sex'] = input_data['Victim Sex'].astype(str)
+        input_data['Victim Descent'] = input_data['Victim Descent'].astype(str)
+        input_data['Premise Code'] = input_data['Premise Code'].astype(str)
+        input_data['Area ID'] = input_data['Area ID'].astype(str)
+        input_data['Weapon Used Code'] = input_data['Weapon Used Code'].astype(str)
+        input_data['Time Occurred'] = input_data['Time Occurred'].astype(str)
+        input_data['MO Codes'] = input_data['MO Codes'].astype(str)
+        input_data['Status Code'] = input_data['Status Code'].astype(str)
+
+        input_processed = preprocessor.transform(input_data)
+
+        prediction = model.predict(input_processed)
+        crime_code = prediction[0]
+        crime_description = crime_code_dict.get(crime_code, "Unknown Crime Code")
+        return crime_description
+    
+    st.write('Status Description: ')
+    st.write('Adult Other - osoba dorosła była zaangażowana w incydent')
+    st.write('Invest Cont - śledztwo w sprawie tego przestępstwa jest nadal w toku')
+    st.write('Adult Arrest - osoba dorosła została aresztowana w związku z tym incydentem')
+    st.write('Juv Arrest - osoba niepełnoletnia została aresztowana w związku z tym przestępstwem')
+    st.write('Juv Other - osoba niepełnoletnia była zaangażowana w incydent')
+    st.write('UNK - nieznane')
+
+    with st.form("crime_prediction_form"):
+        st.write("Please enter the details:")
+        victim_age = st.selectbox('Victim Age', labels)
+        victim_sex = st.selectbox('Victim Sex', ['M', 'F'])
+        victim_descent_desc = st.selectbox('Victim Descent', victim_descent_unique)
+        premise_desc = st.selectbox('Premise Description', list(premise_dict.values()))
+        area_name = st.selectbox('Area Name', list(area_dict.values()))
+        weapon_desc = st.selectbox('Weapon Description', list(weapon_dict.values()))
+        time_occurred = st.number_input('Time Occurred', min_value=0, max_value=2359, step=1)
+        mo_desc = st.multiselect('MO Codes', mo_codes_unique)
+        status_desc = st.selectbox('Status Description', status_desc_unique)
+        submitted = st.form_submit_button("Predict Crime Code")
+
+        if submitted:
+            result = predict_crime(victim_age, victim_sex, victim_descent_desc, premise_desc, area_name, weapon_desc, time_occurred, mo_desc, status_desc)
+            typewriter(text=f'The predicted Crime Code is: :rainbow[{result}]', speed=5)
+
     
